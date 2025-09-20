1dfda88ebcbae8e9b73ced08998ab61d20e77c9a
Wafa Roslan <wabroslan1@sheffield.ac.uk>
2025-09-20 11:21:02 +0800

    First commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 
 import './App.css'
+import {FormControl, Container, Button, InputGroup } from "react-bootstrap";
+
+// Declare variables from .env
+const clientId = import.meta.env.VITE_CLIENT_ID;
+const clientSecret = import.meta.env.VITE_CLIENT_SECRET;
+
 
 function App() {
   
 
   return (
    <>
-      <h1>Codédex Project Template</h1>
+    <Container>
+      <InputGroup>
+        <FormControl
+          placeholder="Search for Artist"
+          type="input"
+          aria-label="Search for Artist"
+          onKeyDown={(e) => {}}//Search Function
+          onChange={(e) => {}} //setSearch
+          style={{
+            width: "300px",
+            height: "35px",
+            borderWidth: "0px",
+            borderStyle: "solid",
+            borderRadius: "5px",
+            marginRight: "10px",
+            paddingLeft: "10px",
+          }}
+        />
+
+        <Button onClick={() => {}}>Search</Button>
+
+      </InputGroup>
+    </Container>
+
+
     </>
   )
 }
